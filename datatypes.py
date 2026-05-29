495af32ad9426e834a445be754d6092067d71e3e
Swamy420 <149225698+Swamy420@users.noreply.github.com>
2026-05-29 16:28:56 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,49 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #data types
+>>> 
+>>> a=1
+>>> type(a)
+<class 'int'>
+>>> a=.12
+>>> type(a)
+... 
+<class 'float'>
+>>> a='hii'
+>>> type(a)
+... 
+<class 'str'>
+>>> a="hi"
+>>> type(a)
+... 
+<class 'str'>
+>>> a='''hlo'''
+>>> type(a)
+... 
+<class 'str'>
+>>> a=1+2j
+>>> type(a)
+... 
+<class 'complex'>
+>>> a=2j
+>>> type(a)
+... 
+<class 'complex'>
+>>> a=2i
+SyntaxError: invalid decimal literal
+>>> a=2j+12
+>>> type(a)
+... 
+<class 'complex'>
+>>> a=True
+>>> type(a)
+... 
+<class 'bool'>
+>>> a=False
+>>> type(a)
+... 
+<class 'bool'>
+>>> a="True"
+>>> type(a)
+
+<class 'str'>
